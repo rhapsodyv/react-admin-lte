daa68505615c8c065f8056cf957b9a1a98dd3ba6
Victor M Oliveira <rhapsodyv@gmail.com>
2017-08-12 23:13:27 -0300

    tree do admin lte

--- vendor/AdminLTE.js
+++ vendor/AdminLTE.js
@@ -1,5 +1,6 @@
 const AdminLTESelectors = {
-    pushMenuButton: '[data-toggle="push-menu"]'
+    pushMenuButton: '[data-toggle="push-menu"]',
+    tree: '[data-widget="tree"]'
 };
 
 /* global $ */
@@ -7,10 +8,17 @@ export default class AdminLTE {
     constructor () {
         this.pushMenu = $.fn.layout;
         this.layout = $.fn.layout;
+        this.tree = $.fn.tree;
+        this.controlSidebar = $.fn.controlSidebar;
     }
 
     init() {
         this.pushMenu.call($(AdminLTESelectors.pushMenuButton));
         this.layout.call($('body'), 'init');
+        //tree
+        $(AdminLTESelectors.tree).each(function () {
+            this.tree.call($(this));
+        })
+        // this.controlSidebar parece que não precisa de init, pois só escuta o evento do botao
     }
 }
